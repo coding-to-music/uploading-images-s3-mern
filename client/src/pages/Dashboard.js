9670efc3d33eabd686201764a5cd2f9eb3d9e001
lynn7mitchell <lynn7mitchell@gmail.com>
2020-10-15 21:54:17 -0500

    sending image to the backend/changing computers

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,7 +5,9 @@ import axios from "axios";
 
 export class Dashboard extends Component {
   state = {
-    user: {}
+    user: {},
+    // description: "",
+    selectedFile: null
   };
   componentWillMount() {
     const token = localStorage.getItem("example-app");
@@ -16,20 +18,46 @@ export class Dashboard extends Component {
 
     axios
       .get("api/user")
-      .then(response => {
+      .then((response) => {
         this.setState({
-          user: response.data
+          user: response.data,
         });
       })
-      .catch(err => console.log(err.response));
+      .catch((err) => console.log(err.response));
   }
   handleLogout = () => {
     localStorage.removeItem("example-app");
     this.setState({
-      redirect: true
+      redirect: true,
     });
   };
+  handleSelectedFile = e => {
+    e.preventDefault();
+    this.setState({
+      // description: e.target.value,
+      selectedFile: e.target.files[0]
+    });
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleUpload = event => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    data.append("file", this.state.selectedFile);
+console.log(data)
+    axios
+      .post('/api/upload', data)
+      .then(() => {
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        alert("Oops some error happened, please try again");
+      });
+  };
   render() {
+    const { description, selectedFile } = this.state;
     return (
       <div>
         {/* <i className="material-icons account-icon">account_circle</i> */}
@@ -39,6 +67,30 @@ export class Dashboard extends Component {
           </button>
         </Link>
         <h1>Dashboard</h1>
+
+        <form onSubmit={this.handleUpload}>
+                    <div>
+                      <label htmlFor="description">Description:</label>
+                      <input
+                        type="text"
+                        name=""
+                        onChange={this.onChange}
+                        placeholder="Description"
+                      />
+                    </div>
+
+                    <div>
+                      <input
+                        type="file"
+                        name=""
+                        id=""
+                        onChange={this.handleSelectedFile}
+                      />
+                    </div>
+                    <button type="submit">
+                      Upload
+                    </button>
+                  </form>
       </div>
     );
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,157 +1,209 @@
 module.exports = function (app) {
-    const db = require("../../models");
-    const passport = require('passport');
-    const bcrypt = require('bcryptjs');
-    const jwt = require('jsonwebtoken');
-    const keys = require('../../config/keys')
-    
-    // Test Routes
-    app.get("/users/noAuthTest", (req, res)=>{
-        res.json({
-            msg: "Users route works (no auth)"
-        })
-    })
+  const db = require("../../models");
+  const passport = require("passport");
+  const bcrypt = require("bcryptjs");
+  const jwt = require("jsonwebtoken");
+  const keys = require("../../config/keys");
+  require("dotenv").config();
 
-    app.get("/users/test", passport.authenticate('jwt', { session: false }), (req, res) => {
-        res.json({
-            msg: "users routes works!"
-        });
+  const multer = require("multer");
+  var multerS3 = require('multer-s3')
+  var AWS = require("aws-sdk");
+  var s3 = new aws.S3({ /* ... */ })
+  var storage = multer.memoryStorage();
+  var upload = multer({ storage: storage });
+
+ 
+
+  // Test Routes
+  app.get("/users/noAuthTest", (req, res) => {
+    res.json({
+      msg: "Users route works (no auth)",
     });
+  });
 
-    // GET ROUTE
+  app.get(
+    "/users/test",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      res.json({
+        msg: "users routes works!",
+      });
+    }
+  );
 
-    app.get('/api/user', passport.authenticate('jwt', {session: false}), (req, res)=>{
+  // GET ROUTE
 
-        db.User.findById(req.user.id)
-        .then(user =>{
-            if(user){
-                res.status(200).json(user)
-            }
+  app.get(
+    "/api/user",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      db.User.findById(req.user.id)
+        .then((user) => {
+          if (user) {
+            res.status(200).json(user);
+          }
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
+    }
+  );
 
-    })
+  //Create new user POST ROUTE
 
-
-
-    //Create new user POST ROUTE
-
-    app.post('/api/user', (req, res) => {
-        db.User.findOne({ email: req.body.email })
-            .then(user => {
-                if (user) {
-                    return res.status(400).json({
-                        email: 'This email already exists'
-                    });
-                } else {
-                    const newUser = {
-                        firstName: req.body.firstName,
-                        lastName: req.body.lastName,
-                        email: req.body.email,
-                        password: req.body.password
-                    }
-                    console.log(newUser)
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            // if (err) throw err;
-                            newUser.password = hash;
-
-                            db.User.create(newUser)
-                                .then(user => {
-                                    res.status(200).json({
-                                        message: "User account successfully created.",
-                                        userCreated: true
-                                    })
-                                })
-                                .catch(err => console.log(err))
-                        })
-                    })
-                }
-            })
-
-
-
-
-
-
-    })
-
-
-    // LOGIN
-
-    app.post('/api/user/login', (req, res) => {
-        const { email, password } = req.body;
-        console.log(email)
-        db.User.findOne({ email: email })
-            .then(user => {
-                // console.log("********************",user)
-                if (!user) {
-                    return res.status(404).json({ user: "User not found" })
-                }
-                // let currentUser = user.get()
-                // TypeError: Cannot read property 'replace' of undefined
-                bcrypt.compare(password, user.password)
-                    .then(isMatch => {
-                        if (isMatch) {
-                            // console.log("bycrypt user", user._id)
-                            db.User.findById(user._id)
-                                .then(user => {
-                                    // console.log("###############", user)
-                                    const payload = {
-                                        id: user.id,
-                                        email: user.email,
-                                        firstName: user.firstName,
-                                        lastName: user.lastName
-                                    }
-                                    jwt.sign(
-                                        payload,
-                                        keys.secretOrKey,
-                                        { expiresIn: 3600 * 12 },
-                                        (err, token) => {
-                                            res.json({
-                                                ...payload,
-                                                success: true,
-                                                token: `Bearer ${token}`
-                                                // GOOD FOR 12 HOURS
-                                            });
-                                        }
-                                    )
-
-                                }).catch(err => console.log(err))
-                        } else {
-                            return res.status(400).json({
-                                password: 'User password could not be validated'
-                            })
-                        }
-                    }).catch(err => console.log(err))
-
-            }).catch(err => console.log(err));
-
-
+  app.post("/upload", upload.single("file"), function(req, res) {
+    const file = req.file;
+    const s3FileURL = process.env.AWS_UPLOADED_FILE_URL_LINK;
   
+    let s3bucket = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION
+    });
+  
+    console.log(process.env.AWS_ACCESS_KEY_ID);
+    console.log(process.env.AWS_SECRET_ACCESS_KEY);
+  
+    //Where you want to store your file
+  
+    var params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+      ACL: "public-read"
+    };
+  console.log(params)
+    s3bucket.upload(params, function(err, data) {
+      if (err) {
+        res.status(500).json({ error: true, Message: err });
+      } else {
+        res.send({ data });
+        var newFileUploaded = {
+        //   description: req.body.description,
+          fileLink: s3FileURL + file.originalname,
+          s3_key: params.Key
+        };
+        var document = new DOCUMENT(newFileUploaded);
+        document.save(function(error, newFile) {
+          if (error) {
+            throw error;
+          }
+        });
+      }
+    });
+  });
+  
+
+  // LOGIN
+
+  app.post("/api/user/login", (req, res) => {
+    const { email, password } = req.body;
+    console.log(email);
+    db.User.findOne({ email: email })
+      .then((user) => {
+        // console.log("********************",user)
+        if (!user) {
+          return res.status(404).json({ user: "User not found" });
+        }
+        // let currentUser = user.get()
+        // TypeError: Cannot read property 'replace' of undefined
+        bcrypt
+          .compare(password, user.password)
+          .then((isMatch) => {
+            if (isMatch) {
+              // console.log("bycrypt user", user._id)
+              db.User.findById(user._id)
+                .then((user) => {
+                  // console.log("###############", user)
+                  const payload = {
+                    id: user.id,
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                  };
+                  jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    { expiresIn: 3600 * 12 },
+                    (err, token) => {
+                      res.json({
+                        ...payload,
+                        success: true,
+                        token: `Bearer ${token}`,
+                        // GOOD FOR 12 HOURS
+                      });
+                    }
+                  );
+                })
+                .catch((err) => console.log(err));
+            } else {
+              return res.status(400).json({
+                password: "User password could not be validated",
+              });
+            }
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
+
+  // PUT
+
+  app.put(
+    "/api/user",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      console.log("put route", req.body);
+      db.User.findByIdAndUpdate(req.user.id, { $set: req.body })
+        .then((user) => {
+          res.status(200).json({
+            message: "User account successfully created.",
+            userCreated: true,
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  );
+
+
+  app.post('/api/upload', upload.single('file'), (req,res)=>{
+    console.log('**********************************'+ req +'*****************************')
+    const file = req.file
+    const s3FileURL = process.env.AWS_UPLOADED_FILE_URL_LINK
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
     })
 
+    var params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read"
+      };
 
+      s3bucket.upload(params, function(err, data) {
+        if (err) {
+          res.status(500).json({ error: true, Message: err });
+        } else {
+          res.send({ data });
+          var newFileUploaded = {
+            description: req.body.description,
+            fileLink: s3FileURL + file.originalname,
+            s3_key: params.Key
+          };
+          var document = new DOCUMENT(newFileUploaded);
+          document.save(function(error, newFile) {
+            if (error) {
+              throw error;
+            }
+          });
+        }
+      });
+})
 
-
-    // PUT
-
-    app.put('/api/user', passport.authenticate('jwt', { session: false }), (req, res) => {
-        console.log("put route", req.body)
-        db.User.findByIdAndUpdate(req.user.id, {$set:req.body})
-        .then(user => {
-            res.status(200).json({
-                message: "User account successfully created.",
-                userCreated: true
-            })
-        })
-        .catch(err => console.log(err))
-        
-    });
-
-
-
-
-
-    // end
-}
+  // end
+};
